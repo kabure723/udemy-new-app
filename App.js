fa147a87ad4f042fd5008bacdff5317932aea2c9
futa shibuya <>
2021-04-08 08:30:18 +0900

    24-画面を表示する

--- App.js
+++ App.js
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +9,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    backgroundColor: "red",
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: "blue",
+    flex: 1
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style= {styles.itemContainer}>
+        <View style= {styles.leftContainer}/>
+        <View style= {styles.rightContainer}/>
+      </View>
+    </View>
+  )
+}
