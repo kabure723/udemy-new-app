3c5f65c2167eedb850cfbbd52c1a0706364286f9
futa shibuya <>
2021-04-09 06:04:17 +0900

    左のエリアに画像を追加

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -30,7 +30,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style= {styles.itemContainer}>
-        <View style= {styles.leftContainer}/>
+        <View style= {styles.leftContainer}>
+        <Image
+        style={{ width: 100, height: 100}}
+        source={{
+          uri: "https://picsum.photos/id/10/200/200",
+        }}
+        />
+        </View>
         <View style= {styles.rightContainer}/>
       </View>
     </View>
