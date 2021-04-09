621e430de188092f2569a3385b3fd28f1522c7e4
futa shibuya <>
2021-04-10 06:27:00 +0900

    APII経由で繋いでみよう

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from "./conponants/Listitem";
+import dummyArticles from "./damy/articles.json";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   itemContainer: {
     height: 100,
@@ -34,26 +38,33 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [articles , setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, [])
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style= {styles.itemContainer}>
-        <View style= {styles.leftContainer}>
-        <Image
-        style={{ width: 100, height: 100}}
-        source={{
-          uri: "https://picsum.photos/id/10/200/200",
-        }}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item })=> (
+        <ListItem
+        imageUrl={item.urlToImage}
+        title={item.title}
+        author={item.author}
         />
-        </View>
-        <View style= {styles.rightContainer}>
-          <Text numberOfLines={3} style= {styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style= {styles.subText}>
-            ReactNews
-          </Text>
-        </View>
-      </View>
-    </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   )
 }
